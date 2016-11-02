007b8ff7558d06130d91d420a062007fd0234251
ume-r <chanzxryox1015@gmail.com>
2016-11-02 22:47:42 +0900

    devise導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  has_many: :chat_group_users
-  has_many: :chat_groups, through: :chat_group_users
-  has_many: :messages
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :chat_group_users
+  has_many :chat_groups, through: :chat_group_users
+  has_many :messages
 end
