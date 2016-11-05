e69a636f84553ceed5e8df6fa1e15ec2bcef58e3
ume-r <chanzxryox1015@gmail.com>
2016-11-05 19:15:26 +0900

    サインイン後のパスを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     '/users/sign_in'
   end
+
+  def after_sign_in_path_for(resource)
+    '/chat_groups'
+  end
 end
