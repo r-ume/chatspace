c8e85413eca8c98e295836303371dad71fa945ab
ume-r <chanzxryox1015@gmail.com>
2016-11-21 00:09:12 -0500

    ログインに名前を登録できるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   add_flash_types :success, :info, :warning, :danger
 
   def after_sign_out_path_for(resource)
@@ -12,4 +14,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     '/chat_groups'
   end
+
+  # ------- private function --------
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: [:name]
+  end
+
+
 end
