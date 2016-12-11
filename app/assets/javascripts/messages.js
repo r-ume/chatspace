0f771da26ada76b2265f8d5c09ac43d2dd30cb91
ume-r <chanzxryox1015@gmail.com>
2016-12-11 16:03:22 +0900

    余白余行削除とコメント追加

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -8,21 +8,13 @@ $(function(){
         var h = created_at.getHours();
         var mi = created_at.getMinutes();
         var created_at_format = ' ' + y + '年' + m + '月' + d + '日' + h + '時' + mi + '分';
-
         var sent_by_who = ' ' + 'sent by' + ' '+ data.name;
-        console.log(sent_by_who);
-
         var image = '<img src =' + data.image + '>';
-        console.log(image);
-
         if(data.image){
             var html = $('<li class = "messages">').append(data.body, sent_by_who, created_at_format, image);
         } else{
             var html = $('<li class = "messages">').append(data.body, sent_by_who, created_at_format);
         }
-
-        console.log(html);
-
         return html;
     }
 
@@ -30,37 +22,27 @@ $(function(){
         e.preventDefault();
         //まずは、js-form__text-fieldのエリア指定
         var textField = $('.js-form__text-field');
+        //まずは、form__image__fieldのエリア指定
         var imageField = $('.form_image_field');
-        // その中身を取得
-        // var message = textField.val();
 
         // jsonのurl指定
         // window.location.hrefは、現在のページのurlを取得するもの
         var jsonUrl = window.location.href;
 
         //　フォーム送信（メッセージを送信する場合は)、formDataクラスの初期化が必要。
+        // 今までは、本文と画像のそれぞれを送信しようとしていたが、それでは、画像を送信するときに、file apiを使わないといけないので、
+        // フォームをまとめて、送信するように設定する。
         // http://ginpen.com/2013/05/07/jquery-ajax-form/
         var message = new FormData($(this).get(0));
-        // var $form = $(this);
-        // var message = new FormData(document.forms.namedItem("the-form"));
 
         //ここからが大事
         $.ajax({
             //　新しく作るので、post
             type: 'POST',
             // urlは、上で定義したもの
-            // 上のpostやgetで動くurlを指定する
+            // 上のPOSTやGETで動くurlを指定する -> ここはよーく復習
             url: jsonUrl,
-            // ここの書き方重要　よーく復習
             data: message,
-            // data: {
-            //     message: {
-            //         user_name: message,
-            //         body: message,
-            //         created_at: message,
-            //         image_url: message
-            //     }
-            // },
             dataType: 'json',
             processData: false,
             contentType: false
@@ -68,7 +50,9 @@ $(function(){
             .done(function(data){
                 var html = buildHTML(data);
                 $('.messages_list').append(html);
+                //　本文を送信したあと、textFieldに本文が残っているので、textfieldを空にする。
                 textField.val('');
+                // 画像を送信したあと、textFieldに画像が残っているので、imageFieldを空にする。
                 imageField.val('');
             })
             .fail(function(){
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :chat_group
   belongs_to :user
-
-  # validates :body, presence: true, length: { minimum: 1 }
-  # validates :image, presence: true
-
   mount_uploader :image, ImageUploader
-
   #　この書き方、重要
   def json_api
     {
