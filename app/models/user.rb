d89545b47f7798ce7a6e5fd4590c37753c303dcc
r-ume <baymax.blackandwhite@gmail.com>
2017-08-07 10:52:39 +0900

    added validation to name column and uniquness to email column

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   has_many :chat_group_users
   has_many :chat_groups, through: :chat_group_users
   has_many :messages
+
+  validates :name,  presence: true
+  validates :email, uniqueness: true
 end
