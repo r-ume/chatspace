e1b55871cd80861921ba32c55c0781eb6cdb0d37
r-ume <baymax.blackandwhite@gmail.com>
2017-08-07 12:57:20 +0900

    made a FactoryGirl data for Message Model

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
-
   factory :message do
-    text 'rspec_message'
-    chat_group_id 1
-    user_id 1
-    image 'rspec.jpg'
+    body             { Faker::Pokemon.name } #just for fun
+    image            { Faker::Avatar.image }
+    chat_group_id    1
+    user_id          1
   end
 end
