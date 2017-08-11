c3df68285cbfbe2d31ee19e6a87bde87077fee7e
r-ume <baymax.blackandwhite@gmail.com>
2017-08-11 13:25:06 +0900

    added tests of message#create

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
   describe '#GET index' do
-    let(:chat_group) { create(:chat_group) }
-    let(:index_params) { {chat_group_id: chat_group} }
+    let(:chat_group)    { create(:chat_group) }
+    let(:index_params)  { { chat_group_id: chat_group } }
+    let(:create_params) { { chat_group_id: chat_group, message: attributes_for(:message) } }
 
     context 'when an user is signed in' do
       login_user
@@ -38,31 +39,40 @@ RSpec.describe MessagesController, type: :controller do
   end
 
   describe 'GET #create' do
-    context 'ログインしている場合' do
-      context 'メッセージの保存に成功' do
-        it 'メッセージの保存ができたか' do
-
+    context 'when an user is signed in' do
+      login_user
+      context 'when an message get saved' do
+        it 'checks if an message can be saved' do
+          # user is defined within login_user method
+          message = build(:message, chat_group_id: chat_group, user_id: user)
+          expect(message).to be_valid
         end
 
-        it '意図した画面に遷移しているのか' do
-
+        it 'redirects to chat_group_messages_path' do
+          message = create(:message)
+          post :create, params: create_params
+          expect(response).to redirect_to chat_group_messages_path
         end
       end
 
-      context 'メッセージの保存に失敗' do
-
-        it 'メッセージの保存は行われなかったか' do
-
+      context 'when an message fails to be saved' do
+        it 'sees if an message does not get saved' do
+          message = build(:message, body: "", image: "")
+          expect(message).to be_invalid
         end
 
         it '意図したビューが描画されているか' do
-
+          message = build(:message, body: "", image: "")
+          post :create, params: create_params
+          expect(response).to redirect_to group_messages_path
         end
       end
     end
 
-    context 'ログインしていない場合' do
-      it '意図した画面にリダイレクトされているのか' do
+    context 'when an user is not signed in' do
+      it 'renders new session template' do
+        post :create, params: create_params
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
