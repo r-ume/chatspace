65da0a8e87fc96d89e06aff96f296497905daabb
r-ume <baymax.blackandwhite@gmail.com>
2017-08-11 12:51:02 +0900

    let annotate gem make a list of properties in each model

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id            :integer          not null, primary key
+#  body          :text(65535)
+#  chat_group_id :integer
+#  user_id       :integer
+#  image         :string(255)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
