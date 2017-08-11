bf82ce088023d835bb3c1caf0954d7d6ff0e79c5
r-ume <baymax.blackandwhite@gmail.com>
2017-08-11 17:13:11 +0900

    fixed all the tests of messages#create action

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -37,47 +37,35 @@ RSpec.describe MessagesController, type: :controller do
       end
     end
   end
-end
 
-  # describe 'GET #create' do
-#     let(:chat_group)    { create(:chat_group) }
-#     let(:create_params) { { chat_group_id: chat_group, message: attributes_for(:message) } }
-#
-#     context 'when an user is signed in' do
-#       login_user
-#       context 'when an message get saved' do
-#         it 'checks if an message can be saved' do
-#           # user is defined within login_user method
-#           message = build(:message, chat_group_id: chat_group, user_id: controller.current_user)
-#           expect(message).to be_valid
-#         end
-#
-#         it 'redirects to chat_group_messages_path' do
-#           message = create(:message)
-#           post :create, params: create_params
-#           expect(response).to redirect_to chat_group_messages_path
-#         end
-#       end
-#
-#       context 'when an message fails to be saved' do
-#         it 'sees if an message does not get saved' do
-#           message = build(:message, body: "", image: "")
-#           expect(message).to be_invalid
-#         end
-#
-#         it '意図したビューが描画されているか' do
-#           message = build(:message, body: "", image: "")
-#           post :create, params: create_params
-#           expect(response).to redirect_to group_messages_path
-#         end
-#       end
-#     end
-#
-#     context 'when an user is not signed in' do
-#       it 'renders new session template' do
-#         post :create, params: create_params
-#         expect(response).to redirect_to new_user_session_path
-#       end
-#     end
-#   end
-# end
+  describe 'GET #create' do
+    let(:chat_group)    { create(:chat_group) }
+    let(:create_params) { { chat_group_id: chat_group, message: attributes_for(:message) } }
+
+    context 'when an user is signed in' do
+      login_user
+
+      it 'checks if an message can be saved' do
+        message = build(:message, chat_group_id: chat_group, user_id: controller.current_user.id)
+        expect(message).to be_valid
+      end
+
+      it 'redirects to chat_group_messages_path' do
+        post :create, create_params
+        expect(response).to redirect_to chat_group_messages_path
+      end
+
+      it 'sees if an message does not get saved' do
+        message = build(:message, body: "", image: "")
+        expect(message).to be_invalid
+      end
+    end
+
+    context 'when an user is not signed in' do
+      it 'renders new session template' do
+        post :create, create_params
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+end
