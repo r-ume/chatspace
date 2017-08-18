56b9cc33323b4a720688ed81ef7998fe0e21799f
r-ume <baymax.blackandwhite@gmail.com>
2017-08-18 11:22:34 +0900

    divided tests into ones with valid attributes and others with invalid attributes

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -40,31 +40,56 @@ RSpec.describe MessagesController, type: :controller do
   end
 
   describe 'GET #create' do
+    let(:user)          { create(:user) }
     let(:chat_group)    { create(:chat_group) }
-    let(:create_params) { { chat_group_id: chat_group, message: attributes_for(:message) } }
+    let(:valid_create_params) { {
+        chat_group_id: chat_group.id, user_id: user.id, message: attributes_for(:message)
+    } }
+    let(:invalid_create_params) { {
+        chat_group_id: chat_group.id, user_id: nil, message: attributes_for(:message, body: nil, image: nil)
+    } }
 
-    context 'when an user is signed in' do
+    describe 'when an user is signed in' do
       login_user
+      context 'with valid attributes' do
+        before do
+          post :create, valid_create_params
+        end
 
-      it 'checks if an message can be saved' do
-        message = build(:message, chat_group_id: chat_group, user_id: controller.current_user.id)
-        expect(message).to be_valid
+        it 'checks if an message can be saved' do
+          expect{ post :create, valid_create_params }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to chat_group_messages_path' do
+          post :create, valid_create_params
+          expect(response).to redirect_to chat_group_messages_path
+        end
+
+        it 'has the flash message' do
+          expect(flash[:notice]).to eq 'successfully sent'
+        end
       end
 
-      it 'redirects to chat_group_messages_path' do
-        post :create, create_params
-        expect(response).to redirect_to chat_group_messages_path
-      end
+      context 'with invalid attributes' do
+        before do
+          post :create, invalid_create_params
+        end
 
-      it 'sees if an message does not get saved' do
-        message = build(:message, body: "", image: "")
-        expect(message).to be_invalid
+        it 'sees if an message does not get saved' do
+          expect{ post :create, invalid_create_params }.not_to change(Message, :count)
+          # message = build(:message, body: "", image: "")
+          # expect(message).to be_invalid
+        end
+
+        it 'redirects to chat_group_messages_path' do
+          expect(response).to redirect_to chat_group_messages_path
+        end
       end
     end
 
-    context 'when an user is not signed in' do
+    describe 'when an user is not signed in' do
       it 'renders new session template' do
-        post :create, create_params
+        post :create, valid_create_params
         expect(response).to redirect_to new_user_session_path
       end
     end
