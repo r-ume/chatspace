78f24bd72bca0a8fe035ec2488a31d692c713c2e
r-ume <baymax.blackandwhite@gmail.com>
2017-09-06 00:37:49 +0900

    redefined the tables structure on database

--- db/migrate/20161012002106_create_chatgroups.rb
+++
@@ -1,8 +0,0 @@
-class CreateChatgroups < ActiveRecord::Migration
-  def change
-    create_table :chatgroups do |t|
-      t.string :name
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,44 +14,59 @@
 ActiveRecord::Schema.define(version: 20161102133716) do
 
   create_table "chat_group_users", force: :cascade do |t|
-    t.integer  "chat_group_id", limit: 4
-    t.integer  "user_id",       limit: 4
+    t.integer  "chat_group_id", limit: 4, null: false
+    t.integer  "user_id",       limit: 4, null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
 
+  add_index "chat_group_users", ["chat_group_id", "user_id"], name: "index_chat_groups_users_on_chat_group_id_and_user_id", using: :btree
+  add_index "chat_group_users", ["chat_group_id", "user_id"], name: "uk_chat_group_users_on_chat_group_id_and_user_id", unique: true, using: :btree
+  add_index "chat_group_users", ["user_id"], name: "fk_rails_689bd81e0d", using: :btree
+
   create_table "chat_groups", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",       limit: 255, default: "", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
+
+  add_index "chat_groups", ["name"], name: "index_chat_groups_name", using: :btree
+  add_index "chat_groups", ["name"], name: "uk_chat_groups_name", unique: true, using: :btree
 
   create_table "messages", force: :cascade do |t|
-    t.text     "body",          limit: 65535
-    t.integer  "chat_group_id", limit: 4
-    t.integer  "user_id",       limit: 4
-    t.string   "image",         limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.text     "body",          limit: 65535,              null: false
+    t.integer  "chat_group_id", limit: 4,     default: 0,  null: false
+    t.integer  "user_id",       limit: 4,     default: 0,  null: false
+    t.string   "image",         limit: 255,   default: "", null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
   end
 
+  add_index "messages", ["chat_group_id", "user_id"], name: "index_messages_on_chat_group_id_and_user_id", using: :btree
+  add_index "messages", ["user_id"], name: "fk_rails_273a25a7a6", using: :btree
+
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255
-    t.string   "encrypted_password",     limit: 255
-    t.string   "name",                   limit: 255
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
+    t.string   "name",                   limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0, null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "chat_group_users", "chat_groups"
+  add_foreign_key "chat_group_users", "users"
+  add_foreign_key "messages", "chat_groups"
+  add_foreign_key "messages", "users"
 end
