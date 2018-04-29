a1bb48bd7066cd8be016984546b2001163310d3d
romukey <baymax.blackandwhite@gmail.com>
2018-04-29 22:38:50 +0900

    changed hello_react.jsx to app.jsx

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -2,9 +2,9 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const Hello = props => (
   <div class = 'react'>Hello {props.name}!</div>
@@ -23,4 +23,4 @@ document.addEventListener('DOMContentLoaded', () => {
     <Hello name="React" />,
     document.body.appendChild(document.createElement('div')),
   )
-})
+});
